01c46f7090356c9fa30021a25be5412c1118191c
Patrick D Faerber <patrickdfaerber@gmail.com>
2023-12-17 19:46:29 -0500

    Add commentary and compare user choice to winner

--- main.py
+++ main.py
@@ -1,33 +1,39 @@
-# import random
-# rand_integer = (random.randint(1, 11))
-# print(rand_integer)
-
-# random_float = random.random() * 5
-# print(random_float)
-# print(my_data[1])
-# display art
 import random
-
 from art import logo as logo
 from art import vs as vs
 from game_data import data as my_data
 
-# load the choices
+#print art
 print(logo)
+
+#first random selection
 rand_person1 = random.choice(my_data)
 print(f"{rand_person1.get('name')}, Followers: {rand_person1.get('follower_count')}")
+
+#print art
 print(vs)
+
+#second random selection
 rand_person2 = random.choice(my_data)
-print(f"{rand_person2.get('name')}, Followers: {rand_person2.get('follower_count')}")
+print(f"{rand_person2.get('name')}, Followers: {rand_person2.get('follower_count')}\n")
 
-choice1 = int(rand_person1.get('follower_count'))
-print(choice1)
-choice2 = int(rand_person2.get('follower_count'))
-print(choice2)
+#prompt user for input
+user_choice = int(input("Choose which persona has the most social media followers. \n" +
+      "Enter 1 or 2 for your choice: \n"))
+#print(user_choice)
 
-if choice2 > choice1:
-    winner = choice2
+#compare random selections and assign to winner
+if int(rand_person1.get('follower_count')) > int(rand_person2.get('follower_count')):
+    winner = 1
+    print(f"{rand_person1.get('name')} wins with {rand_person1.get('follower_count')} followers!")
 else:
-    winner = choice1
-
+    winner = 2
+    print(f"{rand_person2.get('name')} wins with {rand_person2.get('follower_count')} followers!")
 print(winner)
+
+#compare user choice to winner
+if user_choice == winner:
+    print("You win! Keep guessing: \n")
+else:
+    print("Sorry :( Better luck next time.")
+
